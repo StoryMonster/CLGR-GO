3da3d6682485e4ab4ee363dab518dcd59ae2c52b
dochen <StoryMonster@aliyun.com>
2019-10-21 11:53:28 +0800

    增加时间花费计算

--- src/output/default.go
+++ src/output/default.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"os"
 	"fmt"
+	"time"
 	"../common"
 )
 
@@ -11,10 +12,11 @@ type DefaultOutput struct {
 	writer io.Writer
 	matchedFilesNum int   // 文本搜索或者文件搜索匹配到的文件或文件夹数量
 	matchedLinesNum int   // 文本搜索时匹配到的文本行数
+	startTime time.Time   // 用于记录程序搜索花费的时间
 }
 
 func NewDefaultOutput() *DefaultOutput {
-	return &DefaultOutput{os.Stdout, 0, 0}
+	return &DefaultOutput{os.Stdout, 0, 0, time.Now()}
 }
 
 func (do *DefaultOutput) write(str string) {
@@ -43,5 +45,8 @@ func (do *DefaultOutput) SearchConclusion() {
 	if do.matchedLinesNum > 0 {
 		str += fmt.Sprintf("  Matched lines: %d", do.matchedLinesNum)
 	}
-	do.writer.Write([]byte(str+"\n"))
+	str += "\n"
+	endTime := time.Now()
+	str += fmt.Sprintf("Time cost: %fs\n", endTime.Sub(do.startTime).Seconds())
+	do.writer.Write([]byte(str))
 }
